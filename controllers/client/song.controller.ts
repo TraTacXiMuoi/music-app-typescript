d8ca0adfd19f2b39760f41de3b49d0a11d70c4fe
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2024-03-17 23:18:49 +0700

    5. Làm trang danh sách bài hát (bên client) - 2

--- controllers/client/song.controller.ts
+++ controllers/client/song.controller.ts
@@ -1,10 +1,35 @@
 import { Request, Response } from "express";
+import Topic from "../../models/topic.model";
+import Song from "../../models/song.model";
+import Singer from "../../models/singer.model";
 
 // [GET] /songs/:slugTopic
 export const list = async (req: Request, res: Response) => {
-  console.log(req.params.slugTopic);
+  const slugTopic: string = req.params.slugTopic;
+
+  const topic = await Topic.findOne({
+    slug: slugTopic,
+    deleted: false,
+    status: "active"
+  });
+
+  const songs = await Song.find({
+    topicId: topic.id,
+    deleted: false,
+    status: "active"
+  }).select("avatar title singerId like slug");
+
+  for (const song of songs) {
+    const infoSinger = await Singer.findOne({
+      _id: song.singerId,
+      deleted: false
+    });
+
+    song["infoSinger"] = infoSinger;
+  }
 
   res.render("client/pages/songs/list", {
-    pageTitle: "Danh sách bài hát",
+    pageTitle: topic.title,
+    songs: songs
   });
 };
