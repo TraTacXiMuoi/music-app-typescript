c8e0bf2f7949f6cb085828ce694b7776b1fbd964
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2024-03-23 22:33:06 +0700

    3. Trang Chỉnh sửa bài hát

--- index.ts
+++ index.ts
@@ -6,6 +6,7 @@ import adminRoutes from "./routes/admin/index.route";
 import { systemConfig } from "./config/system";
 import path from "path";
 import bodyParser from "body-parser";
+import methodOverride from "method-override";
 
 
 dotenv.config();
@@ -15,6 +16,8 @@ const app: Express = express();
 const port: string | number = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(methodOverride('_method'));
 
 app.set("views", "./views");
 app.set("view engine", "pug");
